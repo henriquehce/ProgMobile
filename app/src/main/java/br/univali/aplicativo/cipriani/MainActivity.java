0ffcfb210e075787b1c568da55a972dc2834e1b3
Henrique Cipriani Ertal <henriquecipriani@edu.univali.br>
2024-09-06 20:41:09 -0300

    Primeiro Commit

--- app/src/main/java/br/univali/aplicativo/cipriani/MainActivity.java
+++ app/src/main/java/br/univali/aplicativo/cipriani/MainActivity.java
@@ -3,12 +3,41 @@ package br.univali.aplicativo.cipriani;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultado;
+    private EditText nota1;
+    private EditText nota2;
+    private Button Calculo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultado = findViewById(R.id.resultado);
+        nota1 = findViewById(R.id.Nota1);
+        nota2 = findViewById(R.id.nota2);
+        Calculo = findViewById(R.id.somar);
+
+        Calculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int n1 = Integer.parseInt(String.valueOf(nota1.getText()));
+                int n2 = Integer.parseInt((String.valueOf(nota2.getText())));
+                int res2 = n1+n2;
+                resultado.setText("Resultado: " + res2);
+
+            }
+        });
+
+
+
     }
 }
